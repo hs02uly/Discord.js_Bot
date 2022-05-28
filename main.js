763b31bce202a115cf70d03f910784e26d8517e6
UMEBOSHIKING <100026529+hs02uly@users.noreply.github.com>
2022-05-28 18:10:53 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,12 +4,22 @@ http.createServer(function (req, res) {
   res.end();
 }).listen(8080);
 
-const fs = require("fs");
+
 const { Client, Intents, Interaction, Collection, Message, MessageEmbed, MessegeButton } = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const fs = require("fs"); //ファイル読み取り
+const colors = require("colors");
+const guild = client.guilds.cache.get("899952674307457056")
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+
+client.on("ready", () => {
+    console.log("Ready!起動しました！")
+    guild.commands.set([])
+        .then(console.log)
+        .catch(console.error); //コマンド一斉削除
+})
 
 let command_int = 0
 for (const file of commandFiles) {
@@ -26,10 +36,10 @@ client.on("interactionCreate", async interaction => {
     if (!interaction.isCommand()) return;
     const command = client.commands.get(interaction.commandName);
     if (!command) return;
-    try {
+    try { //例外が発生するかも
         await command.execute(interaction);
     } catch (error) {
-        console.error(error);
+        console.error(error); //したときの処理
         await interaction.reply({ content: "コマンド実行時にエラーが発生しました。", ephemeral: true});
     }
 });
